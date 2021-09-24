830c65597eb93be5536c47d972b836a5ecb19ca0
dev-swd <tladnjsen@gmail.com>
2021-09-24 23:57:51 +0900

    [2021-09-24] 4.9-event-part1 - props 이벤트 메서드 호출 테스트

--- src/component/habits.jsx
+++ src/component/habits.jsx
@@ -1,24 +1,44 @@
+/** @format */
+
 import React, { Component } from 'react';
 import Habit from './habit';
 
 class Habits extends Component {
   state = {
     habits: [
-      { id : 1, name : 'Reading', count : 0 },
-      { id : 2, name : 'Running', count : 0 },
-      { id : 3, name : 'Coding', count : 0 },
-    ],
-  }
+      { id: 1, name: 'Reading', count: 0 },
+      { id: 2, name: 'Running', count: 0 },
+      { id: 3, name: 'Coding', count: 0 }
+    ]
+  };
+
+  handleHabitIncrease = (habit) => {
+    console.log('---');
+    console.log(habit);
+  };
+
+  handleHabitDecrease = (habit) => {
+    console.log(habit);
+  };
+
+  handleHabitDelete = (habit) => {
+    console.log(habit);
+  };
 
   render() {
     return (
       <ul>
-        {this.state.habits.map(habit => (
-          <Habit key={habit.id} habit={habit} />
+        {this.state.habits.map((habit) => (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={this.handleHabitIncrease}
+            onDecrement={this.handleHabitDecrease}
+            onDelete={this.handleHabitDelete}
+          />
         ))}
       </ul>
-    )
-
+    );
   }
 }
 
