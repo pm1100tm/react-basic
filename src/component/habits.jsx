1095da3daaac83a4e4ec32146f3ef45dfa294141
dev-swd <tladnjsen@gmail.com>
2021-09-25 01:46:01 +0900

    [2021-09-24] 4.10-11 - 4.10-11 완료 - App.jsx 에서 function app 을 App component 로 변경 - Navbar 추가

--- src/component/habits.jsx
+++ src/component/habits.jsx
@@ -4,44 +4,16 @@ import React, { Component } from 'react';
 import Habit from './habit';
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: 'Reading', count: 0 },
-      { id: 2, name: 'Running', count: 0 },
-      { id: 3, name: 'Coding', count: 0 }
-    ]
-  };
-
-  handleHabitIncrease = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-    this.setState({ habits });
-  };
-
-  handleHabitDecrease = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count < 0 ? 0 : count;
-    this.setState({ habits });
-  };
-
-  handleHabitDelete = (habit) => {
-    const habits = this.state.habits.filter((item) => item.id !== habit.id);
-    this.setState({ habits });
-  };
-
   render() {
     return (
       <ul>
-        {this.state.habits.map((habit) => (
+        {this.props.habits.map((habit) => (
           <Habit
             key={habit.id}
             habit={habit}
-            onIncrement={this.handleHabitIncrease}
-            onDecrement={this.handleHabitDecrease}
-            onDelete={this.handleHabitDelete}
+            onIncrement={this.props.onIncrement}
+            onDecrement={this.props.onDecrement}
+            onDelete={this.props.onDelete}
           />
         ))}
       </ul>
---
+++ src/component/navbar.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <i className="navbar-logo fas fa-leaf"></i>
+        <span>Habit Tracker</span>
+        <span className="navbar-count">{this.props.habitCount}</span>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
