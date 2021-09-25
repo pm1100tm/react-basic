b79dd7521cc67a2a0cd3109adf00b7733d3b71f3
dev-swd <tladnjsen@gmail.com>
2021-09-25 17:14:10 +0900

    [2021-09-25] 4.12 - habit-add.jsx 추가

--- src/component/habits.jsx
+++ src/component/habits.jsx
@@ -2,21 +2,29 @@
 
 import React, { Component } from 'react';
 import Habit from './habit';
+import HabbitAdd from './habit-add';
 
 class Habits extends Component {
+  onAddHabit = (event) => {
+    console.log('Habits');
+  };
+
   render() {
     return (
-      <ul>
-        {this.props.habits.map((habit) => (
-          <Habit
-            key={habit.id}
-            habit={habit}
-            onIncrement={this.props.onIncrement}
-            onDecrement={this.props.onDecrement}
-            onDelete={this.props.onDelete}
-          />
-        ))}
-      </ul>
+      <>
+        <HabbitAdd />
+        <ul>
+          {this.props.habits.map((habit) => (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.props.onDecrement}
+              onDelete={this.props.onDelete}
+            />
+          ))}
+        </ul>
+      </>
     );
   }
 }
