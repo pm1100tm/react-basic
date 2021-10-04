7a8e76b927ce3ca681fd9a4e65ccb3c95733a753
dev-swd <tladnjsen@gmail.com>
2021-10-04 23:49:44 +0900

    <Feture/4.18> React Hook - useState, useCallback, useEffect, useRef

--- src/component/simpleHabit.jsx
+++ src/component/simpleHabit.jsx
@@ -1,29 +1,55 @@
 /** @format */
 
-import React, { Component } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
-class SimpleHabit extends Component {
-  state = {
-    count: 0
-  };
+const SimpleHabit = (props) => {
+  const [count, setCount] = useState(0);
+  // const spanRef = useRef();
 
-  handleIncrease = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+  const handleIncrease = useCallback(() => {
+    setCount(count + 1);
+  });
 
-  render() {
-    return (
-      <>
-        <li className="habit-li">
-          <span className="habit-name">Reading</span>
-          <span className="habit-count">{this.state.count}</span>
-          <button className="habit-button habit-increase" onClick={this.handleIncrease}>
-            <i className="fas fa-plus-square"></i>
-          </button>
-        </li>
-      </>
-    );
-  }
-}
+  useEffect(() => {
+    console.log(`mounted & updated ${count}`);
+  }, []);
+
+  return (
+    <li className="habit-li">
+      {/* ref={spanRef} */}
+      <span className="habit-name">Reading</span>
+      <span className="habit-count">{count}</span>
+      <button className="habit-button habit-increase" onClick={handleIncrease}>
+        <i className="fas fa-plus-square"></i>
+      </button>
+    </li>
+  );
+};
 
 export default SimpleHabit;
+
+// class SimpleHabit extends Component {
+//   state = {
+//     count: 0
+//   };
+
+//   handleIncrease = () => {
+//     this.setState({ count: this.state.count + 1 });
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <li className="habit-li">
+//           <span className="habit-name">Reading</span>
+//           <span className="habit-count">{this.state.count}</span>
+//           <button className="habit-button habit-increase" onClick={this.handleIncrease}>
+//             <i className="fas fa-plus-square"></i>
+//           </button>
+//         </li>
+//       </>
+//     );
+//   }
+// }
+
+// export default SimpleHabit;
